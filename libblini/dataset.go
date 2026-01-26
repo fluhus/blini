a4177092d817626b06cef8d6cff5b3df1c50a91a
fluhus <amitlavon1.spam@gmail.com>
2026-01-26 09:34:54 -0800

    Add check for nil index in clean function

--- libblini/dataset.go
+++ libblini/dataset.go
@@ -63,6 +63,9 @@ func (d *Dataset[T]) SearchSketch(s *Sketch[T], contn bool) iter.Seq[SearchResul
 // CleanIndex removes singletons from the index.
 // Used in clustering operations to reduce memory consumption.
 func (d *Dataset[T]) CleanIndex() {
+	if d.idx == nil {
+		panic("this dataset was created with no search index")
+	}
 	d.idx.Clean()
 }
 
