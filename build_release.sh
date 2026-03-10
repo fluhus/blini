344e35140e3ef542b83de7ef48d7ea50348e4d3f
fluhus <amitlavon1.spam@gmail.com>
2026-03-10 15:08:45 -0700

    Add blini2 to build script

--- build_release.sh
+++ build_release.sh
@@ -2,27 +2,38 @@
 
 set -e
 
-EXE=blini
 VERSION=v1.1.0
+VERSION2=v2.0.0
+
+EXE=blini
 OUTDIR=../release
+TAGS=
+SUFFIX=
+
+if [ "$1" == "2" ]; then
+  VERSION=$VERSION2
+  TAGS="-tags=blini2"
+  SUFFIX=2
+fi
+
 FLAGS="-ldflags=-s -X main.version=$VERSION"
 
 rm -fr $OUTDIR
 mkdir $OUTDIR
 
 # Linux
-go build "$FLAGS" -o $OUTDIR ./$EXE
-zip -j $OUTDIR/${EXE}_linux.zip $OUTDIR/$EXE
+go build $TAGS "$FLAGS" -o $OUTDIR ./$EXE
+zip -j $OUTDIR/${EXE}${SUFFIX}_linux.zip $OUTDIR/$EXE
 rm $OUTDIR/$EXE
 
 # Mac
-GOOS=darwin go build "$FLAGS" -o $OUTDIR ./$EXE
-zip -j $OUTDIR/${EXE}_mac.zip $OUTDIR/$EXE
+GOOS=darwin go build $TAGS "$FLAGS" -o $OUTDIR ./$EXE
+zip -j $OUTDIR/${EXE}${SUFFIX}_mac.zip $OUTDIR/$EXE
 rm $OUTDIR/$EXE
 
 # Windows
-GOOS=windows go build "$FLAGS" -o $OUTDIR ./$EXE
-zip -j $OUTDIR/${EXE}_win.zip $OUTDIR/$EXE.exe
+GOOS=windows go build $TAGS "$FLAGS" -o $OUTDIR ./$EXE
+zip -j $OUTDIR/${EXE}${SUFFIX}_win.zip $OUTDIR/$EXE.exe
 rm $OUTDIR/$EXE.exe
 
 ls -lh $OUTDIR
